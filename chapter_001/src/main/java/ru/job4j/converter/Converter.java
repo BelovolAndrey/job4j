23a8d554a575cae9fb6e998dcf6ac565ff6c1e68
BelovolAndrey <belovol.andrey@gmail.com>
2019-12-02 21:01:29 +0300

    JavaDoc. Code convention[#202808]

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -1,19 +1,46 @@
 package ru.job4j.converter;
 
+/**
+ * Калькулятор выводит резульат операций на экран
+ */
 public class Converter {
 
+    /**
+     * Конвертирует рубль в евро
+     *
+     * @param value - значение конвертации
+     * @return - возвращаемый результат
+     */
     public static int rubleToEuro(int value) {
         return value / 70;
     }
 
+    /**
+     * Конвертирует рубль в доллар
+     *
+     * @param value - значение конвертации
+     * @return - возвращаемый результат
+     */
     public static int rubleToDollar(int value) {
         return value / 60;
     }
 
+    /**
+     * Конвертирует евро в рубль
+     *
+     * @param value - значение конвертации
+     * @return - возвращаемый результат
+     */
     public static int euroToRub(int value) {
         return value * 70;
     }
 
+    /**
+     * Конвертирует доллар в рубль
+     *
+     * @param value - значение конвертации
+     * @return - возвращаемый результат
+     */
     public static int dollarToRub(int value) {
         return value * 60;
     }
@@ -24,23 +51,23 @@ public class Converter {
         int expected = 2;
         int out = rubleToEuro(eroChek);
         boolean passed = expected == out;
-        System.out.println("140 rubles are " + euro + " euro. " +passed);
+        System.out.println("140 rubles are " + euro + " euro. " + passed);
 
         int dollarCheck = 120;
         int outDollarCheck = rubleToDollar(dollarCheck);
         boolean passedDollar = expected == outDollarCheck;
-        System.out.println("120 rubles are " + outDollarCheck + " dollar. " +passedDollar);
+        System.out.println("120 rubles are " + outDollarCheck + " dollar. " + passedDollar);
 
         int euroRubCheck = 3;
-        int  outEuroRubCheck= euroToRub(euroRubCheck);
+        int outEuroRubCheck = euroToRub(euroRubCheck);
         int expectedEuroRubCheck = 210;
         boolean passedEuroRubCheck = expectedEuroRubCheck == outEuroRubCheck;
-        System.out.println("3 euro are " + outEuroRubCheck + " rub. " +passedEuroRubCheck);
+        System.out.println("3 euro are " + outEuroRubCheck + " rub. " + passedEuroRubCheck);
 
         int dollarRubCheck = 4;
-        int  outDollarRubCheck= dollarToRub(dollarRubCheck);
+        int outDollarRubCheck = dollarToRub(dollarRubCheck);
         int expectedDollarRubCheck = 240;
         boolean passedDollarRubCheck = outDollarRubCheck == expectedDollarRubCheck;
-        System.out.println("4 dollar are " + outDollarRubCheck + " rub. " +passedDollarRubCheck);
+        System.out.println("4 dollar are " + outDollarRubCheck + " rub. " + passedDollarRubCheck);
     }
 }
