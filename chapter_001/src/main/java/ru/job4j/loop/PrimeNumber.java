4229ce5c7bc0be7d4ac0c22dec3e1b842fb68586
BelovolAndrey <belovol.andrey@gmail.com>
2019-12-03 18:28:10 +0300

    Простые числа.[#202811]_v2

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -1,19 +1,14 @@
 package ru.job4j.loop;
 
-import java.util.BitSet;
-import java.util.LinkedList;
-
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
-        LinkedList<Integer> primes = new LinkedList<Integer>();
-        BitSet nonPrimes = new BitSet(finish + 1);
-
-        for (int p = 2; p <= finish; p = nonPrimes.nextClearBit(p + 1)) {
-            for (int i = p * p; i <= finish; i += p)
-                nonPrimes.set(i);
-            primes.add(p);
+        CheckPrimeNumber cpn = new CheckPrimeNumber();
+        for (int i = 2; i <= finish; i++) {
+            if (cpn.check(i)) {
+                count++;
+            }
         }
-        return primes.size();
+        return count;
     }
 }
