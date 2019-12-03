130e06a354846481e0da4bd7ae28cae29378ec25
BelovolAndrey <belovol.andrey@gmail.com>
2019-12-03 18:51:31 +0300

    Ипотека.[#202812]_v2

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,21 +3,13 @@ package ru.job4j.loop;
 public class Mortgage {
     public int year(int amount, int salary, double percent) {
         int year = 0;
-        boolean isComplite = false;
         double realAmount = amount * (1 + percent / 100);
-        while (!isComplite) {
-            if (realAmount > 0) {
-                if (salary - realAmount < 0) {
-                    realAmount -= salary;
-                    year++;
-                } else {
-                    year++;
-                    isComplite = true;
-                }
-            } else {
-                isComplite = true;
-            }
-        }
+        do {
+            amount = (int) (realAmount - salary);
+            realAmount = amount * (1 + percent / 100);
+            year++;
+
+        } while (realAmount > 0);
         return year;
     }
 }
