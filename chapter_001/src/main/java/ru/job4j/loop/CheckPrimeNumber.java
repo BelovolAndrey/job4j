ca789704a4022a1128fa60e158d5f6864bcd73f2
BelovolAndrey <belovol.andrey@gmail.com>
2019-12-03 18:09:51 +0300

    Простое число.[#202813]_v2

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,20 +2,12 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public boolean check(int finish) {
-        boolean prime = false;
-        if (finish == 0) {
-            return false;
-        }
-        if (finish == 1) {
-            return true;
-        } else if (finish == 2) {
-            return true;
-        }
+        boolean prime = true;
         for (int i = 2; i < finish; i++) {
             if (finish % i == 0) {
-                return false;
+                prime = false;
             }
         }
-        return true;
+        return prime;
     }
 }
