102740a352ef53ac5857e13260912dc3421917b3
BelovolAndrey <belovol.andrey@gmail.com>
2019-12-08 16:44:45 +0300

    Слово заканчивается с ...[#202827]_v2

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -2,13 +2,13 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
+        boolean result = true;
         // проверить. что массив word имеет последние элементы одинаковые с post
-        String someWord = new String(word);
-        String checkWord = new String(post);
-        String tempWord = someWord.substring(someWord.length() - checkWord.length());
-        if (tempWord.equals(checkWord)) {
-            result = true;
+        for (int i = 0; i < post.length; i++) {
+            if (post[post.length - 1 - i] != word[word.length - 1 - i]) {
+                result = false;
+                break;
+            }
         }
         return result;
     }
