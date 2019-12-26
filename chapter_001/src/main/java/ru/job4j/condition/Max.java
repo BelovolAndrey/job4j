9ec286a8e771f9e08889c5f70d38d9082d0954b8
BelovolAndrey <belovol.andrey@gmail.com>
2019-12-26 14:42:21 +0300

    3. Перегрузить метод max для трех чисел[#202943]_v2

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -7,33 +7,14 @@ public class Max {
     }
 
     public static int max(int a, int b, int c) {
-        int result = 0;
-        if (a > b && a > c) {
-            result = a;
-        }
-        if (b > a && b > c) {
-            result = b;
-        }
-        if (c > a && c > b) {
-            result = c;
-        }
-        return result;
+        int result = a > b ? a : b;
+        return result > c ? result : c;
     }
 
     public static int max(int a, int b, int c, int d) {
-        int result = 0;
-        if (a > b && a > c && a > d) {
-            result = a;
-        }
-        if (b > a && b > c && b > d) {
-            result = b;
-        }
-        if (c > a && c > b && c > d) {
-            result = c;
-        }
-        if (d > a && d > b && d > c) {
-            result = d;
-        }
+        int result = a > b ? a : b;
+        result = result > c ? result : c;
+        result = result > d ? result : d;
         return result;
     }
 }
