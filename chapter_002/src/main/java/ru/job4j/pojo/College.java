260d4c0e4148269f7c4e2ed533ace892c4af263d
BelovolAndrey <belovol.andrey@gmail.com>
2019-12-27 13:57:33 +0300

    2. Модель данных.[#202965]

---
+++ chapter_002/src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setSurname("Иванов");
+        student.setName("Иван");
+        student.setPatronymic("Иванович");
+        student.setGroup("Группа 3");
+        student.setReceiptDate(new Date());
+        System.out.println("ФИО: " + student.getSurname() + " " + student.getName() + " " + student.getPatronymic() +
+                " Группа: " + student.getGroup() + " Дата поступления: " + student.getReceiptDate());
+    }
+}
