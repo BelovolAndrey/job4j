0ec581c16e66806541f5e40ce1b95a2701acce85
BelovolAndrey <belovol.andrey@gmail.com>
2020-01-03 13:25:08 +0300

    5.1. Тестирование. Подготовка данных.[#202929]

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -3,6 +3,7 @@ package ru.job4j.tracker;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
@@ -31,5 +32,28 @@ public class TrackerTest {
         tracker.add(item);
         Item[] result = tracker.findAll();
         assertThat(result.length, is(1));
+    }
+
+    @Test
+    public void whenDelete() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item("Bug");
+        Item bug2 = new Item("bug");
+        tracker.add(bug);
+        tracker.add(bug2);
+        String id = bug.getId();
+        tracker.delete(id);
+        assertThat(tracker.findById(id), is(nullValue()));
+    }
+
+    @Test
+    public void whenReplace(){
+        Tracker tracker = new Tracker();
+        Item bug = new Item("Bug");
+        Item bug2 = new Item("bug");
+        tracker.add(bug);
+        String id = bug.getId();
+        tracker.replace(id,bug2);
+        assertThat(tracker.findById(id), is(bug2));
     }
 }
