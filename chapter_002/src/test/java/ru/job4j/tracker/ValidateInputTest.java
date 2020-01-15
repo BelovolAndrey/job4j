52e8455cb1b9e533e55f0bc2a92a61109acd620c
BelovolAndrey <belovol.andrey@gmail.com>
2020-01-15 20:20:16 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#202931]_v2

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -9,20 +11,45 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class ValidateInputTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream stdout = System.out;
+
+    @Before
+    public void loadOutput() {
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+    }
 
     @Test
     public void whenInvalidInput() {
-        ByteArrayOutputStream mem = new ByteArrayOutputStream();
-        PrintStream out = System.out;
-        System.setOut(new PrintStream(mem));
+//        ByteArrayOutputStream mem = new ByteArrayOutputStream();
+//        PrintStream out = System.out;
+//        System.setOut(new PrintStream(mem));
         ValidateInput input = new ValidateInput(
                 new StubInput(new String[]{"one", "1"})
         );
         input.askInt("Enter");
         assertThat(
-                mem.toString(),
+                out.toString(),
                 is(String.format("Please enter validate data again.%n"))
         );
-        System.setOut(out);
+        System.setOut(stdout);
+    }
+
+    @Test
+    public void whenIllegalStateInput() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[]{"1", "2"})
+        );
+        input.askInt("3",0);
+        assertThat(
+                out.toString(),
+                is(String.format("Please select key from menu.%n"))
+        );
+        System.setOut(stdout);
     }
 }
