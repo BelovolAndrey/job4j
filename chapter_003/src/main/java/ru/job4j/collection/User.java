e8118af45897f380e9f2433170a5f37b8fab81ef
BelovolAndrey <belovol.andrey@gmail.com>
2020-02-10 12:54:14 +0300

    2. Сортировка User с использованием Comparator[#202881]

--- chapter_003/src/main/java/ru/job4j/collection/User.java
+++ chapter_003/src/main/java/ru/job4j/collection/User.java
@@ -6,6 +6,14 @@ public class User implements Comparable<User> {
     private String name;
     private int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
     public User(String name, int age) {
         this.name = name;
         this.age = age;
--- chapter_003/src/test/java/ru/job4j/collection/UserTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/UserTest.java
@@ -2,11 +2,13 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
 
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -28,5 +30,45 @@ public class UserTest {
                         new User("Ivan", 31)
                 );
         assertThat(rsl, greaterThan(0));
+    }
+
+    @Test
+    public void compareName() {
+        Comparator<User> comName = new UserCompareName();
+        int rsl = comName.compare(
+                new User("Monk", 20),
+                new User("Greg", 30)
+        );
+        assertThat(rsl, lessThan(7));
+    }
+
+    @Test
+    public void compareNameReverse() {
+        Comparator<User> comName = new UserCompareNameReverse();
+        int rsl = comName.compare(
+                new User("Monk", 20),
+                new User("Greg", 30)
+        );
+        assertThat(rsl, lessThan(-1));
+    }
+
+    @Test
+    public void compareAge() {
+        Comparator<User> comName = new UserCompareAge();
+        int rsl = comName.compare(
+                new User("Monk", 20),
+                new User("Greg", 30)
+        );
+        assertThat(rsl, is(-1));
+    }
+
+    @Test
+    public void compareAgeReverse() {
+        Comparator<User> comName = new UserCompareAgeReverse();
+        int rsl = comName.compare(
+                new User("Monk", 20),
+                new User("Greg", 30)
+        );
+        assertThat(rsl, is(1));
     }
 }
