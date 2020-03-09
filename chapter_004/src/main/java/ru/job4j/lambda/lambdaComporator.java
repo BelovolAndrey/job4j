1da790c6c6da63ec171afb5b3701975d32de60f1
BelovolAndrey <belovol.andrey@gmail.com>
2020-03-09 12:50:27 +0300

    //0.2 Зона видимости в лямбда и исключения[#202764]

--- chapter_004/src/main/java/ru/job4j/lambda/lambdaComporator.java
+++ chapter_004/src/main/java/ru/job4j/lambda/lambdaComporator.java
@@ -1,10 +1,23 @@
 package ru.job4j.lambda;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 public class lambdaComporator {
     public static void main(String[] args) {
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
         Comparator<String> cmpDescSize = (left, right) -> left.length() - right.length();
+
+        //0.2 Зона видимости в лямбда и исключения[#202764]
+        // Чтобы избежать любых проблем в лямбда выражениями все переменные нужно обозначать final
+        List<String> names = Arrays.asList("Petr", "Nick", "Ban");
+        final StringBuilder last = new StringBuilder();
+        names.forEach(
+                n ->  {
+                    last.ensureCapacity(0);
+                    last.append(n);
+                }
+        );
     }
 }
