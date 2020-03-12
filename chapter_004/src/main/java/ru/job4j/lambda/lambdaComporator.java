021f9c94af375c709f2daba2dacb8fe5fff00963
BelovolAndrey <belovol.andrey@gmail.com>
2020-03-12 12:12:01 +0300

    0. Функциональные итерфейсы. [#202762]_v2

--- chapter_004/src/main/java/ru/job4j/lambda/lambdaComporator.java
+++ chapter_004/src/main/java/ru/job4j/lambda/lambdaComporator.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class lambdaComporator {
     public static void main(String[] args) {
         Comparator<String> cmpText = (left, right) -> left.compareTo(right);
-        Comparator<String> cmpDescSize = (left, right) -> left.length() - right.length();
+        Comparator<String> cmpDescSize = (left, right) -> right.length() - left.length();
 
         //0.2 Зона видимости в лямбда и исключения[#202764]
         // Чтобы избежать любых проблем в лямбда выражениями все переменные нужно обозначать final
